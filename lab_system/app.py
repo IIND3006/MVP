dee47842fe1c686ebfe01dfbdab235cd11e180e6
mariadeleonchar <157546629+mariadeleonchar@users.noreply.github.com>
2026-05-12 14:23:25 -0500

    Change app.run to use environment PORT variable

--- lab_system/app.py
+++ lab_system/app.py
@@ -339,4 +339,5 @@ def cancel_reservation(reservation_id):
 # ────────────────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
